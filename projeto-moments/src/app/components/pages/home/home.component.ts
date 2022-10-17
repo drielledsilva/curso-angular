46317a97749b4b2ff3aa866f801782228b320294
Drielle da Silva <drielle.silva@live.com>
2022-10-17 19:58:56 -0300

    Update home.component.ts

--- projeto-moments/src/app/components/pages/home/home.component.ts
+++ projeto-moments/src/app/components/pages/home/home.component.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
