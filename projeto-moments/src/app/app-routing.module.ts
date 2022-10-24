1452ced0e0c22a6c7771904046dbd692391ae538
Drielle da Silva <drielle.silva@live.com>
2022-10-24 17:18:38 -0300

    Update app-routing.module.ts

--- projeto-moments/src/app/app-routing.module.ts
+++ projeto-moments/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -7,7 +8,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'moments/new', component: NewMomentComponent}
+  {path: 'moments/new', component: NewMomentComponent},
+  {path: 'moments/:id', component: MomentComponent}
 ];
 
 @NgModule({
